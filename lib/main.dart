1d2294ad159b5b8cc97960e37ef0fd3f1dc74955
riyanari <riyanari611@gmail.com>
2023-06-21 11:48:54 +0700

    setting and initialize date format

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:money_record/config/app_color.dart';
 import 'package:money_record/config/session.dart';
 import 'package:money_record/presentation/pages/auth/login_page.dart';
@@ -8,7 +9,11 @@ import 'package:money_record/presentation/pages/home_page.dart';
 import 'data/model/user.dart';
 
 void main() {
-  runApp(const MyApp());
+  // initializeDateFormatting('id_ID', null).then((value) => null);
+  WidgetsFlutterBinding.ensureInitialized();
+  initializeDateFormatting('id_ID').then((value) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -19,25 +24,20 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light().copyWith(
-        primaryColor: AppColor.primary,
-        colorScheme: const ColorScheme.light(
-          primary: AppColor.primary,
-          secondary: AppColor.secondary
-        ),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: AppColor.primary,
-          foregroundColor: Colors.white
-        )
-      ),
+          primaryColor: AppColor.primary,
+          colorScheme: const ColorScheme.light(
+              primary: AppColor.primary, secondary: AppColor.secondary),
+          appBarTheme: const AppBarTheme(
+              backgroundColor: AppColor.primary,
+              foregroundColor: Colors.white)),
       home: FutureBuilder(
         future: Session.getUser(),
         builder: (context, AsyncSnapshot<User> snapshot) {
-          if (snapshot.data != null && snapshot.data!.idUser!=null){
+          if (snapshot.data != null && snapshot.data!.idUser != null) {
             return const HomePage();
           }
           return const LoginPage();
         },
-
       ),
     );
   }
